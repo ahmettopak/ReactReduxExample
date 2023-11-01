566bf50ada983cb9d7022437bbb66c3135123fc2
Ahmet TOPAK <75997899+ahmettopak@users.noreply.github.com>
2023-11-01 16:45:38 +0300

    Add list

--- src/components/categories/CategoryList.jsx
+++ src/components/categories/CategoryList.jsx
@@ -2,15 +2,23 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import * as categoryActions from '../../redux/actions/categoryActions'
 import { bindActionCreators } from 'redux'
+import { ListGroup, ListGroupItem } from 'reactstrap'
 
 export class CategoryList extends Component {
 
   componentDidMount() {
     this.props.actions.getCategories()
+    console.log(this.props.categories.products.length)
   }
   render() {
     return (
-      <div>CategoryList {this.props.categories.products.length}</div>
+      <div>CategoryList
+        <ListGroup>
+
+          {this.props.categories.products.map(products => (<ListGroupItem key={products.id} > {products.category}</ListGroupItem>
+          ))}
+        </ListGroup>
+      </div >
     )
   }
 }
